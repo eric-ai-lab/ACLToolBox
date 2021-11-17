8e20e81b9988462ab8ee9097ade838dbb7f4c494
Winson Chen <winsonchen108@gmail.com>
2021-11-17 03:35:26 -0800

    cleaning up and adds comments

--- test.py
+++ test.py
@@ -3,27 +3,32 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+
+
 URL = "https://openreview.net/group?id=aclweb.org/ACL/ARR/2021/November"
 
+# get the page by senting request 
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+# beautifulSoup by using lxml to get the raw html and store in soup with different class 
+soup = BeautifulSoup(page.content, "lxml")
 
-# links = soup.find(class="container") 
-
+# find the script that contains the json data
 script = soup.find("script", id="__NEXT_DATA__")
-# print(str(script.string))
+
+# loading the script into json format
 json_object = json.loads(script.string)
 
-# print(json_object['props'])
-
-# test = BeautifulSoup(json_object['props'], "html.parser")
+# using regex for parsing the link of each paper
 documents = re.findall(r"<li><a href=\\\\\"(.+?)\\\\\"", str(json_object['props']))
-title = re.findall(r"\">(.+?)<", str(json_object['props']))
-title = title[10:-2]
-print(len(documents))
-print(len(title))
 
+# using regex for parsing the title of each paper
+title = re.findall(r"\">(.+?)<", str(json_object['props']))
+
+# offset the correct title for each link
+title = title[10:-2]
+
+# combine the title and documents into dictionary
 database = {}
 for x, y in zip(title, documents): 
     database[x] = y
