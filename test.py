2a45f0385e429687dfb70e174abbe23c9c5ddf33
Winson Chen <winsonchen108@gmail.com>
2021-11-18 14:15:42 -0800

    query all months of submissions

--- test.py
+++ test.py
@@ -3,34 +3,57 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+import pandas as pd 
 
 
-URL = "https://openreview.net/group?id=aclweb.org/ACL/ARR/2021/November"
+months = ['October', 'September', 'August', 'July', 'Jun', 'May']
 
-# get the page by senting request 
-page = requests.get(URL)
 
-# beautifulSoup by using lxml to get the raw html and store in soup with different class 
-soup = BeautifulSoup(page.content, "lxml")
+for month in months:
+    print(month)
+    URL = "https://openreview.net/group?id=aclweb.org/ACL/ARR/2021/" + month
 
-# find the script that contains the json data
-script = soup.find("script", id="__NEXT_DATA__")
+    # get the page by senting request 
+    page = requests.get(URL)
 
-# loading the script into json format
-json_object = json.loads(script.string)
+    # beautifulSoup by using lxml to get the raw html and store in soup with different class 
+    soup = BeautifulSoup(page.content, "lxml")
 
-# using regex for parsing the link of each paper
-documents = re.findall(r"<li><a href=\\\\\"(.+?)\\\\\"", str(json_object['props']))
+    # find the script that contains the json data
+    script = soup.find("script", id="__NEXT_DATA__")
 
-# using regex for parsing the title of each paper
-title = re.findall(r"\">(.+?)<", str(json_object['props']))
+    # loading the script into json format
+    json_object = json.loads(script.string)
 
-# offset the correct title for each link
-title = title[10:-2]
+    # using regex for parsing the link of each paper
+    documents = re.findall(r"<li><a href=\\\\\"(.+?)\\\\\"", str(json_object['props']))
 
-# combine the title and documents into dictionary
-database = {}
-for x, y in zip(title, documents): 
-    database[x] = y
+    # using regex for parsing the title of each paper
+    title = re.findall(r"\">(.+?)<", str(json_object['props']))
 
-print(database)
+    print(len(title))
+    print(len(documents))
+
+    # offset the correct title for each link
+    # october [19:-2]
+    # september [10:-2]
+    # August [10:-2]
+    if month == 'October': 
+        title = title[19:-2]
+    else: 
+        title = title[10:-2]
+
+    print(len(title)) 
+    print(len(documents))
+
+    # combine the title and documents into dictionary
+    database = {}
+    for x, y in zip(title, documents): 
+        database[x] = y
+
+    # print(database)
+
+    df = pd.DataFrame.from_dict(database, orient='index', columns=['links'])
+    print(df)
+    df.to_csv(month + '.csv')
+
