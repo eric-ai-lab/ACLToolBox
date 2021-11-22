fb9c3f1cb98632bba747b237bf999d7971927d97
Winson Chen <winsonchen108@gmail.com>
2021-11-21 23:32:55 -0800

    added frequency count, descending frequency

---
+++ data_processing.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+
+
+df = pd.read_csv('abstract_freq.csv')
+df.sort_values(by=['frequency'], ascending=False, ignore_index=True, inplace=True) 
+
+df.to_csv('abstract_freq_desc.csv')
+
