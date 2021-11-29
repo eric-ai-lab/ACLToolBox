eee6a564998115753aa525246a1cf9853a050e65
Winson Chen <winsonchen108@gmail.com>
2021-11-29 14:11:41 -0800

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ my_parser.add_argument('-keyword', metavar='[keyboard]', nargs='+', type=str, he
 args = my_parser.parse_args()
 
 
-months = ['November', 'October', 'September', 'August', 'July', 'June', 'May']
+months = ['November', 'October', 'September', 'August', 'July', 'Jun', 'May']
 
 all = False
 
@@ -20,9 +20,12 @@ keyword = ''
 for word in args.keyword: 
 	keyword += word + ' '
 
-month = args.month
-if month == 'June': 
+month = args.month.capitalize()
+print(month)
+if month == 'June' or month == 'june': 
 	month = 'Jun'
+
+	
 
 index = []
 
@@ -30,7 +33,7 @@ index = []
 
 if month in months: 
 	path = 'csv/' + month 
-elif month == 'all':
+elif month == 'All':
 	all = True 
 else: 
 	print('No month is in the database')
@@ -66,6 +69,9 @@ final_df['pdf'] = 'https://openreview.net'+final_df['pdf']
 final_df['software'] = 'https://openreview.net' + final_df['software']
 final_df['data'] = 'https://openreview.net' + final_df['data']
 
+
+
+print(final_df[['title', 'pdf']].to_string())
 print('saving the result to result.csv')
-# print(final_df)
-final_df.to_csv('result.csv')
+# final_df.to_csv('result.csv')
+
