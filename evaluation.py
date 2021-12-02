521ba9f31c9ecaa7f4e4c98257b42c8bd4b1bce1
Winson Chen <winsonchen108@gmail.com>
2021-12-01 17:31:50 -0800

    updates to correct plural to singular

--- evaluation.py
+++ evaluation.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.stem.regexp import RegexpStemmer
+# from nltk.stem.regexp import RegexpStemmer
+# from nltk.stem.lancaster import LancasterStemmer
 from os.path import exists 
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS
+from pattern.text.en import singularize
+
 
 nltk.download('punkt')
 nltk.download("averaged_perceptron_tagger")
-stemmer = RegexpStemmer('s$|ies$')
-
+# stemmer = RegexpStemmer('s$|ies$')
+# st = LancasterStemmer()
 
 def check_file_exists(filename): 
   if not exists(filename): 
@@ -23,67 +26,73 @@ def add_to_dict(phrase, freq):
   else: 
     freq[phrase] = 1 
 
-def parse_nouns_to_frequency(title, freq):
+def parse_words(title): 
   try:
-    # print(title)
-    title = title.lower()
+    title = title.lower() 
     title_words = word_tokenize(str(title))
-    # print(title_words)
-    words = [stemmer.stem(word) for word in title_words]
-    # print(words)
-    title_tags = nltk.pos_tag(words)
-    grammar = "NP: {<JJ>*<NN>?<NN>?}"
-    chunk_parser = nltk.RegexpParser(grammar)
-    tree = chunk_parser.parse(title_tags)
-    for subtree in tree.subtrees(lambda t: t.label() == 'NP'):
-      # print(subtree.leaves())
-      phrase = ""
-      for i in subtree.leaves(): 
-        phrase += i[0].capitalize() + ' ' 
-      add_to_dict(phrase, freq)
+    return title_words
   except:
-    pass 
+    return None 
+
+def parse_nouns_to_frequency(title, freq):
+  # print(title)
+  title_words = parse_words(title)
+  if not title_words: 
+    return 
+  # print(title_words)
+  words = [singularize(plural) for plural in title_words]
+  # print(words)
+  title_tags = nltk.pos_tag(words)
+  grammar = "NP: {<JJ>*<NN>?<NN>?<NN>?}"
+  chunk_parser = nltk.RegexpParser(grammar)
+  tree = chunk_parser.parse(title_tags)
+  for subtree in tree.subtrees(lambda t: t.label() == 'NP'):
+    # print(subtree.leaves())
+    phrase = ""
+    for i in subtree.leaves(): 
+      phrase += i[0] + ' ' 
+    add_to_dict(phrase, freq)
+
+
+
+
 
 def parse_verbs_to_frequency(title, freq):
-  try:
-    # print(title)
-    title = title.lower()
-    title_words = word_tokenize(str(title))
-    # print(title_words)
-    words = [stemmer.stem(word) for word in title_words]
-    # print(words)
-    title_tags = nltk.pos_tag(words)
-    grammar_v = "V: {<VB>|<VBD>|<VBG>|<VBZ>}"
-    chunk_parser_verb = nltk.RegexpParser(grammar_v)
-    tree_v = chunk_parser_verb.parse(title_tags)
-    for subtree in tree_v.subtrees(lambda t: t.label() == 'V'):
-      # print(subtree.leaves())
-      phrase = ""
-      for i in subtree.leaves(): 
-        phrase += i[0].capitalize() + ' ' 
-      add_to_dict(phrase, freq)
-  except:
-    pass 
+  title_words = parse_words(title)
+  if not title_words:
+    return 
+  title_tags = nltk.pos_tag(title_words)
+  grammar_v = "V: {<VB>|<VBD>|<VBG>|<VBZ>}"
+  chunk_parser_verb = nltk.RegexpParser(grammar_v)
+  tree_v = chunk_parser_verb.parse(title_tags)
+  for subtree in tree_v.subtrees(lambda t: t.label() == 'V'):
+    # print(subtree.leaves())
+    phrase = ""
+    for i in subtree.leaves(): 
+      phrase += i[0] + ' ' 
+    add_to_dict(phrase, freq)
 
 
-def graph_wordcloud(df, name):
+def graph_wordcloud(freq, name):
 	# df = df.rename(columns={'Unnamed: 0': 'phrase', 1: 'frequency'})
-	freq = dict(zip(df['phrase'], df['frequency']))
-	stopwords = set(STOPWORDS) 
+	# freq = dict(zip(df['phrase'], df['frequency']))
 
-	wordcloud = WordCloud(width = 800, height = 800,
-                background_color ='white',
-                stopwords = stopwords,
-                min_font_size = 10).generate_from_frequencies(freq)
+  for key in freq.keys():
+    if key in STOPWORDS:
+      del freq[key]
+  
+  wordcloud = WordCloud(width = 800, height = 800, background_color ='white', min_font_size = 10).generate_from_frequencies(freq)
 
-	plt.figure(figsize = (8, 8), facecolor = None)
-	plt.imshow(wordcloud)
-	plt.axis("off")
-	plt.tight_layout(pad = 0)
+  plt.figure(figsize = (8, 8), facecolor = None)
+  plt.imshow(wordcloud) 
+  plt.axis("off")
+  plt.tight_layout(pad = 0)
 
   # if want to save the pic as png
-	# plt.savefig(name + '.png') 
-	plt.show()	
+  plt.savefig(name + '.png') 
+  plt.show()	
+
+
 
 def main(): 
   dir = "csv/"
@@ -101,8 +110,8 @@ def main():
         
         # iterate through the csv file
         for title in df['title']:
-            # parse_nouns_to_frequency(title, freq)
-            parse_verbs_to_frequency(title, freq)
+            parse_nouns_to_frequency(title, freq)
+            # parse_verbs_to_frequency(title, freq)
           
   # print(freq)
   df = pd.DataFrame(data=freq.values(), index=freq.keys(),columns = ['frequency'])
@@ -116,7 +125,7 @@ def main():
   # print(df)
   # df.to_csv("title_verb_freq.csv")
 
-  graph_wordcloud(df, 'title')
+  graph_wordcloud(freq, 'title')
 
 
 
