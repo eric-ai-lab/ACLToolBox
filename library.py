74906c21dd4aa2e7cfec3239ecc1432d8d9a6bc0
Winson Chen <winsonchen108@gmail.com>
2022-02-07 10:09:05 -0800

    headless

--- library.py
+++ library.py
@@ -5,8 +5,10 @@ import os
 from datetime import datetime 
 from bs4 import BeautifulSoup
 import json 
-import re 
+import re
+import selenium
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
@@ -72,74 +74,77 @@ def create_dir(parent_dir, year):
 
 
 def alternative_scrape(url, month): 
-
-	driver = webdriver.Chrome('./chromedriver')
-	driver.get(url)
-	time.sleep(3)
-	html_source = driver.page_source
-	soup = BeautifulSoup(html_source,'lxml')
-	header = ['id','title','authorids','authors','TL;DR','abstract','pdf','software','preprint','existing_preprints','preferred_venue','consent','paperhash','reviewer/Editor_reassignment_request','reviewer/Editor_reassignment_justification','data','previous_URL','previous_PDF','response_PDF']
+    opts = webdriver.ChromeOptions()
+    opts.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    # driver = webdriver.Chrome('./chromedriver')
+    driver.get(url)
+    # time.sleep(3)
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source,'lxml')
+    header = ['id','title','authorids','authors','TL;DR','abstract','pdf','software','preprint','existing_preprints','preferred_venue','consent','paperhash','reviewer/Editor_reassignment_request','reviewer/Editor_reassignment_justification','data','previous_URL','previous_PDF','response_PDF']
 	
-	df = pd.DataFrame(columns=header)
-	
-	find = soup.find_all("li", {"class": "note"})
-	count = len(find)
-	if count == 0: 
-		return df
-	c = 1
-	printProgressBar(0, count, prefix = month + ' Progress:', suffix = 'Complete', length = 50)
-	for titles in find:
-		printProgressBar(c, count, prefix = month + ' Progress:', suffix = 'Complete', length = 50)
-		c+=1 
-		data = dict()
+    df = pd.DataFrame(columns=header)
 
-		id = titles.get('data-id')
-		if id is None: 
-			continue 
-		# print(id)
+    find = soup.find_all("li", {"class": "note"})
+    count = len(find)
+    if count == 0: 
+        return df
+    c = 1
+    printProgressBar(0, count, prefix = month + ' Progress:', suffix = 'Complete', length = 50)
+    for titles in find:
+        printProgressBar(c, count, prefix = month + ' Progress:', suffix = 'Complete', length = 50)
+        c+=1 
+        data = dict()
+
+        id = titles.get('data-id')
+        if id is None: 
+            continue 
+
+        # print(id)
 		
-		h4 = titles.find('h4')
-		links = h4.find_all('a')
+        h4 = titles.find('h4')
+        links = h4.find_all('a')
 
-		title = " ".join(links[0].text.split())
-		forum = links[0].get('href')
-		pdf = links[1].get('href')
+        title = " ".join(links[0].text.split())
+        forum = links[0].get('href')
+        pdf = links[1].get('href')
 		
-		data['id'] = id 
-		data['title'] = title
-		data['forum'] = forum
-		data['pdf'] = pdf 
+        data['id'] = id 
+        data['title'] = title
+        data['forum'] = forum
+        data['pdf'] = pdf 
 		
 
-		authors = " ".join(titles.find('div', {"class": "note-authors"}).text.split())
-		# print(authors)
-		data['author'] = authors 
+        authors = " ".join(titles.find('div', {"class": "note-authors"}).text.split())
 
-		details = titles.find('ul', {"class": "list-unstyled note-content"})
+        # print(authors)
 
-		items = [" ".join(i.text.split())[:-1] for i in details.find_all('strong', {"class": "note-content-field"})]
-		# print(items)
-		contents = [" ".join(i.text.split()) for i in details.find_all('span', {"class": "note-content-value"})]
-		# print(contents)
-
-		for item, content in zip(items, contents): 
-			try: 
-				if item != 'Previous URL' and item != 'Abstract':
-					t = "Download " + item 
-					content = details.find('a', {"title": t}).get('href')
-				data[item] = content
-			except: 
-				pass
+        data['author'] = authors 
+        details = titles.find('ul', {"class": "list-unstyled note-content"})
+        items = [" ".join(i.text.split())[:-1] for i in details.find_all('strong', {"class": "note-content-field"})]
+        # print(items)
+        contents = [" ".join(i.text.split()) for i in details.find_all('span', {"class": "note-content-value"})]
+        # print(contents)
+        
+        for item, content in zip(items, contents): 
+            try: 
+                if item != 'Previous URL' and item != 'Abstract':
+                    t = "Download " + item 
+                    content = details.find('a', {"title": t}).get('href')
+                    data[item] = content
+            except: 
+                pass
 		
 		# print(data)
 
-		df = df.append(data, ignore_index = True)
+        df = df.append(data, ignore_index = True)
 
 
-		print()
+        print()
 
 	# print(df)
-	return df 
+    return df 
 
 
 
